1dc923423f130fd952e140786ec142f89fb71831
sumanmunde123 <93462136+sumanmunde123@users.noreply.github.com>
2022-04-17 02:57:25 +0530

    react-revision-evaluation

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import "./App.css";
+import { Routes, Route, Navigate, Link } from "react-router-dom";
+import { Home } from "./Components/Home";
+import { AddCountry } from "./Components/AddCountry";
+import { AddCity } from "./Components/AddCity";
+
+function App() {
+  return (
+    <div className="App">
+      <Link to="/"><h3>Home</h3></Link> <br />
+      <Link to="/add-country"><h4>AddCountry</h4></Link> <br /> <br />
+      <Link to="/add-city"><h4>AddCity</h4></Link> <br /> <br />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/add-country" element={<AddCountry />} />
+        <Route path="/add-city" element={<AddCity />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Home.jsx
@@ -0,0 +1,118 @@
+import React, { useEffect, useState } from "react";
+import { styled } from '@mui/material/styles';
+import Table from '@mui/material/Table';
+import Button from '@mui/material/Button';
+import TableBody from '@mui/material/TableBody';
+import TableCell, { tableCellClasses } from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+const StyledTableCell = styled(TableCell)(({ theme }) => ({
+  [`&.${tableCellClasses.head}`]: {
+    backgroundColor: theme.palette.common.black,
+    color: theme.palette.common.white,
+  },
+  [`&.${tableCellClasses.body}`]: {
+    fontSize: 14,
+  },
+}));
+
+const StyledTableRow = styled(TableRow)(({ theme }) => ({
+  '&:nth-of-type(odd)': {
+    backgroundColor: theme.palette.action.hover,
+  },
+  // hide last border
+  '&:last-child td, &:last-child th': {
+    border: 0,
+  },
+}));
+export const Home = () => {
+  const [data, setData] = useState([]);
+  const [sortdata, setSortdata] = useState([]);
+
+  useEffect(() => {
+    fetch(`http://localhost:8000/Country`)
+      .then((res) => res.json())
+      .then((res) => setData(res))
+      .then((res) => setSortdata(res))
+      .catch((res) => console.log(err));
+  }, []);
+
+  //   console.log("data", data);
+const deleteItem = (index) =>{
+  alert("deleted succesfully")
+var newData = data;
+newData.splice(index,1);
+setData([...newData])
+}
+
+
+  const handleescSort=(field,asc=true)=>{
+    const sortdata = data.sort((a,b)=>{
+  
+      if(asc){
+        return a[field]-b[field]
+      }
+     
+        return b[field]-a[field]
+      
+      
+    })
+    setData([...sortdata])
+  }
+  return (
+    
+    <div style={{
+      margin:"100px",
+      marginRight:"250px"
+    }}>
+       <Button style={{color:"black"}} onClick={()=>handleescSort("population")} variant="contained" >
+       Sort by Population
+</Button>
+      
+      <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 700 }} aria-label="customized table">
+        <TableHead>
+          <TableRow>
+           <StyledTableCell>No</StyledTableCell> 
+            <StyledTableCell align="right">Country </StyledTableCell>
+            <StyledTableCell align="right">City </StyledTableCell>
+            <StyledTableCell align="right">Population</StyledTableCell>
+            <StyledTableCell align="right">Edit</StyledTableCell>
+            <StyledTableCell align="right">Delete</StyledTableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {data.map((row,index) => (
+            <StyledTableRow key={row.name}>
+               <StyledTableCell component="th" scope="row"> 
+                <h2>{row.id}</h2>  
+              </StyledTableCell>
+              <StyledTableCell align="right"><h3>{row.country_name}</h3></StyledTableCell>
+              <StyledTableCell align="right"><h3>{row.city_name}</h3></StyledTableCell>
+              <StyledTableCell align="right"><h3>{row.population}</h3></StyledTableCell>
+              <StyledTableCell align="right">
+              <Button variant="contained" color="success">
+edit
+</Button>
+              </StyledTableCell>
+            
+              <StyledTableCell align="right">
+              <Button style={{color:"black", border:"black"}} onClick={()=>deleteItem(index)} variant="outlined" color="error">
+  Delete
+</Button>
+              </StyledTableCell>
+           
+            </StyledTableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+     
+      <br />
+      <br />
+      
+    </div>
+  );
+};
